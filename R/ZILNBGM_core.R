24019fe63b855a0ce9bac50aa7f6fabc413608e8
Park BeomJin <31871763+bbeomjin@users.noreply.github.com>
2019-04-02 15:14:04 +0900

    20190402

--- R/ZILNBGM_core.R
+++ R/ZILNBGM_core.R
@@ -1,4 +1,5 @@
 # NB regression with l1 regularization for x with 1 columns
+# glmreg_fit = mpath:::glmreg_fit
 wlasso_nb = function(y, x, weights, penalty.factor = NULL, eta0 = NULL, mu0 = NULL, theta0 = NULL,
                      lambda, thresh = 1e-6, maxit = 100, n = NROW(x), p = NCOL(x))
 {
@@ -160,9 +161,10 @@ pglm_nb_irls = function(y, x, weights, theta0 = NULL, bvec0 = NULL, eta0 = NULL,
                    lambda, penalty.factor = rep(1, NCOL(x)), thresh = 1e-6, maxit = 1e+3, n = NROW(x), p = NCOL(x))
 {
   fun_call = match.call()
-  negbin_fit = try((penalized_glm(y = y, x = x, weights = weights, lambda = lambda, alpha = 1, theta = theta0, 
-                                  family = "negbin", thresh = thresh, maxit = maxit, penalty.factor = penalty.factor,
-                                  start = bvec0, mustart = mu0, etastart = eta0)), silent = TRUE)
+  negbin_fit = try((glmreg_fit(y = y, x = x, weights = weights, lambda = lambda, alpha = 1, theta = theta0, 
+                               family = "negbin", thresh = thresh, maxit = maxit, penalty.factor = penalty.factor,
+                               start = bvec0, mustart = mu0, etastart = eta0, standardize = FALSE, penalty = "enet",
+                               x.keep = FALSE, y.keep = FALSE, trace = FALSE)), silent = TRUE)
   if (inherits(negbin_fit, "try-error")) {
     negbin_fit = irls_nb(y = y, x = x, weights = weights, lambda = lambda, theta0 = theta0,
                           thresh = thresh, maxit = maxit, penalty.factor = penalty.factor, eta0 = eta0, mu0 = mu0)
--- R/ZILPGM_core.R
+++ R/ZILPGM_core.R
@@ -1,4 +1,5 @@
 # Poisson regression with l1 regularization for x with 1 columns using MM algorithm
+# glmreg_fit = mpath:::glmreg_fit
 wlasso_p = function(y, x, weights, penalty.factor = NULL, eta0 = NULL, mu0 = NULL,
                     lambda, thresh = 1e-6, maxit = 100, n = NROW(x), p = NCOL(x))
 {
@@ -159,9 +160,10 @@ pglm_p_irls = function(y, x, weights, bvec0 = NULL, eta0 = NULL, mu0 = NULL,
                        lambda, penalty.factor = rep(1, NCOL(x)), thresh = 1e-6, maxit = 1e+3, n = NROW(x), p = NCOL(x))
 {
   fun_call = match.call()
-  poisson_fit = try((penalized_glm(y = y, x = x, weights = weights, lambda = lambda, alpha = 1, family = "poisson",
-                                   thresh = thresh, maxit = maxit, penalty.factor = penalty.factor,
-                                   start = bvec0, mustart = mu0, etastart = eta0)), silent = FALSE)
+  poisson_fit = try((glmreg_fit(y = y, x = x, weights = weights, lambda = lambda, alpha = 1, family = "poisson",
+                                thresh = thresh, maxit = maxit, penalty.factor = penalty.factor,
+                                start = bvec0, mustart = mu0, etastart = eta0, standardize = FALSE, penalty = "enet",
+                                x.keep = FALSE, y.keep = FALSE, trace = FALSE)), silent = FALSE)
   if (inherits(poisson_fit, "try-error")) {
     poisson_fit = irls_p(y = y, x = x, weights = weights, lambda = lambda, thresh = thresh, maxit = maxit,
                           penalty.factor = penalty.factor, eta0 = eta0, mu0 = mu0)
